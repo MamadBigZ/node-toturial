4f2e9d9a131616a4d07a75af12ff98df13ad4ba5
salo <iammohamadmahdi@gmail.com>
2026-09-26 18:23:51 +0330

    changes..

--- express/01-express/app.js
+++ express/01-express/app.js
@@ -1,23 +1,24 @@
-const express = require("express");
-const bodyParse = require("body-parser");
 const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
 
 const app = express();
 
-const adminData = require("./routes/admin")
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-const rootDir = require("./utils/path");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(bodyParse.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname , "public")))
-
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-// 404 page
-app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
-});
+app.use(errorController.get404);
 
 app.listen(3001);
--- express/01-express/routes/shop.js
+++ express/01-express/routes/shop.js
@@ -1,14 +1,14 @@
 const path = require("path");
+
 const express = require("express");
 
-const router = express.Router();
+const productsController = require("../controllers/products");
+
+const rootDir = require("../util/path");
 const adminData = require("./admin");
 
-const rootDir = require("../utils/path");
+const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log({product: adminData.products})
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
-});
+router.get("/" , productsController.getProducts);
 
 module.exports = router;
